e99165f4473bce1b294e92f41b587d7fe80663d0
Ben Vierck <me@xcud.com>
2018-08-07 11:06:47 -0500

    drive the bot

--- state_machine.py
+++ state_machine.py
@@ -3,6 +3,16 @@ import redis
 import time
 import json
 import math
+from vision import robot
+
+#sleep_time = 0.015
+sleep_time = 0.015
+
+fluffy = robot.Robot()
+fluffy.speed_low = chr(255)
+fluffy.speed_high = chr(10)
+fluffy.accel_low = chr(255)
+fluffy.accel_high = chr(10)
 
 r = redis.StrictRedis(host="localhost", port=6379, db=0)
 p = r.pubsub(ignore_subscribe_messages=True)
@@ -28,17 +38,19 @@ def meet_the_puck(puck_state, bot_state):
     if d > 100:
         speed = 10
     
-    if abs(d) >5:
+    if abs(d) > 30:
         # calculate the change to the position
         cx = speed * dx/d
         cy = speed * dy/d
         
         new_b = b
-        new_b['x'] -= cx
-        new_b['y'] -= cy
+        new_b['x'] -= int(cx)
+        new_b['y'] -= int(cy)
         
         if new_b['y'] < min_y:
-            new_b['y'] = min_y
+            new_b['y'] = int(min_y)
+
+        fluffy.goto(new_b['x'], new_b['y'])
 
         r.set("machine-state-bot", json.dumps(new_b))
         r.publish('state-changed', True)
@@ -53,4 +65,4 @@ while True:
             puck_state = r.get('machine-state-puck')
             bot_state = r.get('machine-state-bot')
             meet_the_puck(puck_state, bot_state)
-        time.sleep(0.015)
+        time.sleep(sleep_time)
