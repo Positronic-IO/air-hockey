d2704f8074aea89d31c436da07c6460208eed0f5
Ben Vierck <ben@lit.ai>
2018-12-06 08:11:35 -0600

    small puck

--- yolo/train.sh
+++ yolo/train.sh
@@ -1,1 +1,2 @@
 ./darknet detector train cfg/puck.data  cfg/yolo-puck.cfg weights/darknet19_448.conv.23
+#./darknet detector train cfg/small-puck.data  cfg/yolo-small-puck.cfg weights/darknet53.conv.74
