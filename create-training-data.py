6b9b79150635d37c386c69ba8704d8e519a15e36
Ben Vierck <ben@lit.ai>
2018-12-16 13:53:46 -0600

    objectify ui

--- create-training-data.py
+++ create-training-data.py
@@ -3,93 +3,104 @@ from imutils.video import WebcamVideoStream
 import cv2
 from timeit import default_timer as timer
 import numpy as np
-import datetime
 import os
 
-window_name = "science!"
-slider_name = 'Frame'
-cv2.namedWindow(window_name, cv2.WINDOW_FREERATIO)
-cv2.setWindowProperty(window_name, cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_FREERATIO)
-cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+class markers:
+	def __init__(self, frame_count):
+		self.pos_green = self.load(1, frame_count)
+		self.pos_white = self.load(2, frame_count)
+		self.color_green = (26, 253, 141)
+		self.color_white = (255, 255, 255)
 
-vs = cv2.VideoCapture('out.mp4')
-frame_count = int(cv2.VideoCapture.get(vs, int(cv2.CAP_PROP_FRAME_COUNT)))
+	def load(self, index, frame_count):
+		path='pos-{}.npy'.format(str(index))
+		return np.int0(np.load(path)) if os.path.exists(path) else np.zeros((frame_count, 2))
 
-def process_frame(frame):
-	global fps, out, frame_index
-	out = frame
+	def release(self):
+		np.save("pos-1", self.pos_green)
+		np.save("pos-2", self.pos_white)
 
-	if np.sum(pos1[frame_index]) > 0:
-		drawX(pos1[frame_index][0], pos1[frame_index][1], left_color)
-	if np.sum(pos2[frame_index]) > 0:
-		drawX(pos2[frame_index][0], pos2[frame_index][1], right_color)
-	
-	fps_out = "{}/{}".format(str(frame_index), str(frame_count))
-	cv2.putText(out, fps_out, (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255,0, 0), 2)
+	def draw(self, index, frame):
+		draw_these = []
+		if len(self.pos_green) > 0 and np.sum(self.pos_green[index]) > 0:
+			draw_these.append((self.pos_green[index], self.color_green))
+		if len(self.pos_white) > 0 and np.sum(self.pos_white[index]) > 0:
+			draw_these.append((self.pos_white[index], self.color_white))
 
-	cv2.imshow(window_name, out)
+		[self.drawX(frame, m[0][0], m[0][1], m[1]) for m in draw_these]
 
-def on_trackbar(pos, data = None):
-	global frame_index
-	cv2.VideoCapture.set(vs, int(cv2.CAP_PROP_POS_FRAMES), pos)
-	_, frame = vs.read()
-	frame_index = pos
-	process_frame(frame)
+	def drawX(self, out, x, y, color):
+		size=cv2.getTextSize(text="X", fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, thickness=2)
+		cv2.putText(img=out, text="X", org=(x - int(size[0][0] / 2), y + int(size[0][1] / 2)), 
+			fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=color, thickness=2)
 
-cv2.createTrackbar(slider_name, window_name, 1, frame_count, on_trackbar)
-frame_index = -1
+class ui:
+	def __init__(self, video_source):
+		self.window_name = "science!"
+		self.slider_name = 'Frame'
+		self.frame_index = 0
+		self.frame_current = None
 
-def init_pos(index):
-	path = 'pos-{}.npy'.format(str(index))
-	if os.path.exists(path):
-		ret = np.int0(np.load(path))
-	else:
-		ret = np.zeros((frame_count,2))
-	return ret
-pos1 = init_pos(1)
-pos2 = init_pos(2)
-left_color = (26, 253, 141)
-right_color = (255, 255, 255)
+		self.video_source = video_source
+		frame_count = int(cv2.VideoCapture.get(video_source, int(cv2.CAP_PROP_FRAME_COUNT)))
 
-def drawX(x, y, color):
-	global out, window_name
-	text = "X"
-	font_face = cv2.FONT_HERSHEY_PLAIN
-	font_scale = 2
-	thickness = 2
-	size = cv2.getTextSize(text, font_face, font_scale, thickness)
-	cv2.putText(out, text, (x - int(size[0][0] / 2), y + int(size[0][1] / 2)), font_face, font_scale, color, thickness)
-	print(x,y)
+		self.puck_markers = markers(frame_count)
 
-def callback(event, x, y, flags, param):
-	if event == cv2.EVENT_RBUTTONDOWN:
-		pos2[frame_index] = np.int0(np.asarray([x,y]))
-		drawX(x, y, right_color)
-		cv2.imshow(window_name, frame)
+		cv2.namedWindow(self.window_name, cv2.WINDOW_FREERATIO)
+		cv2.setWindowProperty(self.window_name, cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_FREERATIO)
+		cv2.setWindowProperty(self.window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+		cv2.setMouseCallback(self.window_name, self.on_click)
+		cv2.createTrackbar(self.slider_name, self.window_name, 1, frame_count, self.on_trackbar)
 
-	if event == cv2.EVENT_LBUTTONDOWN:
-		if flags & cv2.EVENT_FLAG_CTRLKEY:
-			pos2[frame_index] = np.int0(np.asarray([x,y]))
-			drawX(x, y, right_color)
-			cv2.imshow(window_name, frame)
+	def release(self):
+		cv2.destroyAllWindows()
+		self.puck_markers.release()
+
+	def on_trackbar(self, pos, data=None):
+		cv2.VideoCapture.set(self.video_source, int(cv2.CAP_PROP_POS_FRAMES), pos)
+		_, frame = self.video_source.read()
+		self.frame_index = pos
+		self.process_frame(frame)
+
+	def on_click(self, event, x, y, flags, param):
+		if event == cv2.EVENT_RBUTTONDOWN:
+			self.puck_markers.pos_white[self.frame_index]=np.int0(np.asarray([x, y]))
+
+		if event == cv2.EVENT_LBUTTONDOWN:
+			if flags & cv2.EVENT_FLAG_CTRLKEY:
+				self.puck_markers.pos_white[self.frame_index]=np.int0(np.asarray([x, y]))
+			else:
+				self.puck_markers.pos_green[self.frame_index]=np.int0(np.asarray([x, y]))
+
+		print(x, y)
+		self.puck_markers.draw(self.frame_index, self.frame_current)
+		cv2.imshow(self.window_name, self.frame_current)
+
+	def process_frame(self, frame):
+		cv2.setTrackbarPos(self.slider_name, self.window_name, self.frame_index)
+		self.frame_current = frame
+		self.puck_markers.draw(self.frame_index, frame)
+		cv2.imshow(self.window_name, frame)
+
+def main():
+	vs = cv2.VideoCapture('out.mp4')
+	output = ui(vs)
+
+	while(True):
+		_, frame = vs.read()
+		output.process_frame(frame)
+
+		key = cv2.waitKey(0)
+		if key & 0xFF == ord('q'):
+			break
+		if key & 0xFF == ord('['): # left
+			output.frame_index -= 1
+			cv2.VideoCapture.set(vs, int(cv2.CAP_PROP_POS_FRAMES), output.frame_index)
 		else:
-			pos1[frame_index] = np.int0(np.asarray([x,y]))
-			drawX(x, y, left_color) 
-			cv2.imshow(window_name, frame)
-cv2.setMouseCallback(window_name, callback)
+			output.frame_index += 1
 
-while(True):
-	_, frame = vs.read()
-	frame_index += 1
-	cv2.setTrackbarPos(slider_name, window_name, frame_index) 
+	output.release()
+	vs.release()
 
-	process_frame(frame)
-	
-	if cv2.waitKey(0) & 0xFF == ord('q'):
-		break
-
-np.save("pos-1", pos1)
-np.save("pos-2", pos2)
-
-cv2.destroyAllWindows()
-vs.release()
+if __name__ == "__main__":
+    main()
