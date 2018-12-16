cb46de9f352f10610ad03b75e20a6032d9e372fd
Ben Vierck <ben@lit.ai>
2018-12-16 10:52:37 -0600

    merge

--- yolo/train.sh
+++ yolo/train.sh
@@ -1,2 +1,2 @@
-./darknet detector train cfg/puck.data  cfg/yolo-puck.cfg weights/darknet19_448.conv.23
+./darknet detector train cfg/two-pucks.data  cfg/yolo-two-pucks.cfg weights/darknet19_448.conv.23
 #./darknet detector train cfg/small-puck.data  cfg/yolo-small-puck.cfg weights/darknet53.conv.74
